588a297710141127ba888fcbf0ace0a343a0fae1
Subham Kumar Aich <subham.aich@ideagen.com>
2024-05-03 22:43:00 +0530

    New Form Page

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NewNote from "./components/NewNote";
+import useLocalStorage from "./customHooks/useLocalStorage";
+
+export type Note ={
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type RawNote ={
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES",[]);
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show Id</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
